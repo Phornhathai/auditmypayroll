6c1eec2faa2f1faad44d37d4df50773f12108e63
Aummii Desu <74613199+Phornhathai@users.noreply.github.com>
2024-01-09 16:04:15 +0700

    update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Nopage from "./pages/nopage/Nopage";
 import Menu from "./components/menu/Menu";
-import Submenu from "./components/submenu/Submenu";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
